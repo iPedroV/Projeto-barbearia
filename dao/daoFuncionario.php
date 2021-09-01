48d21b1c6e54019b393f12b29a156b2b9c83f253
abreufabio <abreufabio71@outlook.com>
2021-09-01 09:17:24 -0300

    :jack_o_lantern: Só com os nomes das funções

---
+++ dao/daoFuncionario.php
@@ -0,0 +1,19 @@
+<?php
+#Colocar os includ de acordo com a seu proprio máquina
+
+#Só os nomes das funções por enquanto
+class DaoFuncionario{
+
+    public function inserirFuncionarioDAO(Funcionario $funcionario){
+        
+    }
+
+    public function exluirFuncionarioDAO(Funcionario $funcionario){
+        
+    }
+    
+    public function editarFuncionarioDAO(Funcionario $funcionario){
+        
+    }
+
+}
