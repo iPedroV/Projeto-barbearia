24071e1a03c6189aa5188d0c798cb7df8edd8497
abreufabio <abreufabio71@outlook.com>
2021-09-19 13:47:43 -0300

    Pagina da redefinição de senha :microbe:

---
+++ novasenha.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-bt">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redefinição de Senha</title>
+    <link rel="sorcut icon" href="img/barber-shop.png" type="image/png" style="width: 16px; height: 16px;">
+    <Link rel="stylesheet" href="css/style-novasenha.css">
+</head>
+
+<body>
+    <div id="login">
+        <img src="img/barbearianeves.png" class="imagem">
+        <form method="post">
+            <label for="n-senha">Nova senha:</label>
+            <input type="password" id="n-senha" name="nsenha">
+
+            <label for="c-senha">Confirmar senha:</label>
+            <input type="password" id="c-senha" name="csenha">
+
+            <button type="submit" class="btn efeito-btn" name="e-senha">Confirmar</button>
+        </form>
+    </div>
+</body>
+
+</html>
