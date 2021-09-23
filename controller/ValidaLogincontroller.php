c04f761770771a90cb2dc06927f8ec248527829f
abreufabio <abreufabio71@outlook.com>
2021-09-23 10:41:55 -0300

    Variaveis ok metodo pronto: :microbe:

--- controller/ValidaLogincontroller.php
+++ controller/ValidaLogincontroller.php
@@ -1,45 +1,49 @@
 <?php
-
 ob_start();
 session_start();
-if(!empty($_POST) AND (empty($_POST['email']) OR empty($_POST['senha']))){
+/*if(!empty($_POST) AND (empty($_POST['email']) OR empty($_POST['senha']))){
     header("Location: ../sessionDestroy.php"); exit;
-}
+}*/
 
-include_once '../model/mensagem.php';
-include_once '../dao/daoLogin.php';
-include_once '../model/Clientes.php';
+include_once 'C:/xampp/htdocs/Projeto-barbearia/model/mensagem.php';
+include_once 'C:/xampp/htdocs/Projeto-barbearia/dao/daoLogin.php';
+include_once 'C:/xampp/htdocs/Projeto-barbearia/model/Clientes.php';
 
 if (isset($_POST)){
     $email = $_POST['email'];
     $senha = $_POST['senha'];
+    echo "<script>alert($email)</script>";
 }else{
     header("Location: ../sessionDestroy.php"); exit;
 }
-
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 $daoLogin = new DaoLogin();
 $valcliente = new Clientes();
 
 $valcliente = $daoLogin->validarLoginDAO($email, $senha);
+/*echo gettype($valcliente);*/
+if (gettype($valcliente) == "object") {
+    if (!isset($_SESSION['emailc'])) {
+        //$em = $valcliente->getEmail();
+        //echo "<script>alert('$em')</script>";
+        
+        $_SESSION['emailc'] = $valcliente->getEmail();
+        $_SESSION['idc'] = $valcliente->getId();
+        $_SESSION['nomec'] = $valcliente->getNome();
 
-if(gettype($valcliente) == "object"){
-        if(!isset($_SESSION['email'])){
-            $_SESSION['emailc'] = $valcliente->getEmail();
-            $_SESSION['idc'] = $valcliente->getId();
-            $_SESSION['nomec'] = $valcliente->getNome();
+        $_SESSION['nr'] = rand(1, 1000000);
+        $_SESSION['conferenr'] = $_SESSION['nr'];
 
-            $_SESSION['nr'] = rand(1,1000000);
-            $_SESSION['conferenr'] = $_SESSION['nr'];
-
-            header("Location: ../index.php");
-            exit;
-        }
-}else{
+        header("Location: ../index.php");
+        exit;
+    }
+} else {
     $_SESSION['msg'] = $valcliente;
-    if(isset($_SESSION['emailc'])){
-        $_SESSION['emailc']=null;
-        $_SESSION['idc']=null;
-        $_SESSION['nomec']=null;
+    if (isset($_SESSION['emailc'])) {
+        $_SESSION['emailc'] = null;
+        $_SESSION['idc'] = null;
+        $_SESSION['nomec'] = null;
     }
     header("Location: ../login.php");
     exit;
