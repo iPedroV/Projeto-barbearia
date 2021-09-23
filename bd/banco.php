b72e7927b660f8a09ed2f458a1c8b68401c27a4d
abreufabio <abreufabio71@outlook.com>
2021-09-23 10:41:25 -0300

    Lembrar de alterar a senha do banco :microbe:

--- bd/banco.php
+++ bd/banco.php
@@ -3,7 +3,7 @@
 //Banco Utilizado para poder chamar de forma estruturada a chamada dos Selects com interação
 $servidor = "localhost";
 $usuario = "root";
-$senha = "root";
+$senha = "senac";
 $dbname = "dbbarbearia";
 
 //Criar a conexão
@@ -15,7 +15,7 @@ class Conecta {
         $pdo = null;
         try{
             $pdo = new PDO("mysql:host=localhost;dbname=dbbarbearia", 
-            "root", "root");
+            "root", "senac");
         }catch(Exception $ex){
             echo "<script>alert('Erro na conexão com o "
                 . "banco de dados.')</script>";
