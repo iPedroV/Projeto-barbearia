11d49cc412c11068e0ccb45841dfe96a8d2205a3
abreufabio <abreufabio71@outlook.com>
2021-11-05 11:19:05 -0300

    Mudei o charset que estava errado :microbe:

--- enviar.php
+++ enviar.php
@@ -58,7 +58,7 @@ $assunto = "Recuperação de senha.";
 
 // É necessário indicar que o formato do e-mail é html
 $headers = 'MIME-Version: 1.0' . "\r\n";
-$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 $headers .= 'From: <'.$email.'>';
 //$headers .= "Bcc: $EmailPadrao\r\n";
 
