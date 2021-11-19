2e434735e5cc3299fe73b032c1143b162da95b88
iPedroV <pedro.vcosta405@gmail.com>
2021-11-18 22:04:59 -0300

    Email

--- dao/DaoClientes.php
+++ dao/DaoClientes.php
@@ -5,6 +5,7 @@ include_once 'C:/xampp/htdocs/Projeto-barbearia/bd/banco.php';
 include_once 'C:/xampp/htdocs/Projeto-barbearia/model/Usuario.php';
 include_once 'C:/xampp/htdocs/Projeto-barbearia/model/Mensagem.php';
 include_once 'C:/xampp/htdocs/Projeto-barbearia/VerifyEmail.class.php';
+
 class DaoClientes
 {
 
