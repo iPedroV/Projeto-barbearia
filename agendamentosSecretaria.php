4d69998b8a05b549e1403b9bec36079042ee2889
abreufabio <abreufabio71@oulook.com>
2021-11-25 20:25:45 -0300

    Listagem funcionando :microbe:

--- agendamentosSecretaria.php
+++ agendamentosSecretaria.php
@@ -11,6 +11,22 @@ if ((!isset($_SESSION['emailc']) || !isset($_SESSION['nomec']))
     header("Location: sessionDestroy.php");
     exit;
 }
+
+$id = $_SESSION['idc'];
+include_once 'C:/xampp/htdocs/Projeto-barbearia/controller/agendamentoController.php';
+include_once 'C:/xampp/htdocs/Projeto-barbearia/dao/daoAgendamento.php';
+include_once 'C:/xampp/htdocs/Projeto-barbearia/model/agendamento_model.php';
+include_once 'C:/xampp/htdocs/Projeto-barbearia/model/agendamento_dos_servicos.php';
+
+$dt = new Agendamento();
+$agendamento = new Agendamentos_dos_servicos();
+$dt->setId($agendamento);
+
+$dts = new AgendamentoController();
+
+include_once 'C:/xampp/htdocs/Projeto-barbearia/bd/banco.php';
+include_once 'C:/xampp/htdocs/Projeto-barbearia/model/mensagem.php';
+
 ?>
 
 <!DOCTYPE html>
@@ -39,33 +55,96 @@ if ((!isset($_SESSION['emailc']) || !isset($_SESSION['nomec']))
     </header>
 
     <table class="table">
-        <thead class="thead-dark">
+        <thead>
             <tr>
-                <th scope="col">#</th>
-                <th scope="col">Primeiro</th>
-                <th scope="col">Último</th>
-                <th scope="col">Nickname</th>
+                <th>Data Agendada</th>
+                <th>Horário</th>
+                <th>Forma de Pagamento</th>
+                <th>Valor Total</th>
+                <th>Serviço</th>
+                <th>Funcionario</th>
+                <th>Cliente</th>
             </tr>
         </thead>
         <tbody>
-            <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-            </tr>
-            <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-            </tr>
-            <tr>
-                <th scope="row">3</th>
-                <td>Larry</td>
-                <td>the Bird</td>
-                <td>@twitter</td>
-            </tr>
+            <?php
+            //FUNÇÃO QUE FORMATA A DATA QUE VEM DO MYSQL
+            function vemData($qqdata)
+            {
+                $tempdata = substr($qqdata, 8, 2) . '/' .
+                    substr($qqdata, 5, 2) . '/' .
+                    substr($qqdata, 0, 4);
+                return ($tempdata);
+            }
+
+            //FUNÇÃO QUE FORMATA A HORA QUE VEM DO MYSQL
+            function horaMin($qqdata)
+            {
+                $tempdata = substr($qqdata, 0, 2) . 'h' .
+                    substr($qqdata, 3, 2) . 'min';
+                return ($tempdata);
+            }
+
+            $TabelaVisual = new AgendamentoController();
+            if ($_SESSION['perfilc'] == 'Secretaria') {
+                $listaAgendamento = $TabelaVisual->ListarClienteAgendamento04();
+            }
+            $a = 0;
+            if ($listaAgendamento != null) {
+                foreach ($listaAgendamento as $la) {
+
+                    $a++;
+            ?>
+                    <tr>
+                        <td><?php print_r(vemData($la->getDataAgenda())); ?></td>
+                        <td><?php print_r(horaMin($la->getHorario())); ?></td>
+                        <td><?php print_r($la->getForma_Pagamento()); ?></td>
+                        <td>R$ <?php print_r($la->getValor()); ?></td>
+                        <td>
+                            <?php $result_post = "SELECT * FROM `agendamentos_dos_servicos` "
+                                . "WHERE agendamentos_id = " . $la->getId() . "";
+                            $resultado_post = mysqli_query($conn, $result_post);
+                            while ($row_post = mysqli_fetch_assoc($resultado_post)) {
+                                $serv = $row_post['sf_servicos'];
+
+                                $result_post02 = "Select * from `servicos` WHERE idServicos = $serv";
+                                $resultado_post02 = mysqli_query($conn, $result_post02);
+                                while ($row_post02 = mysqli_fetch_assoc($resultado_post02)) {
+                                    echo '<li style="list-style: none;">' . $row_post02['nome'] . '</li>';
+                                }
+                            } ?>
+                        </td>
+                        <td>
+                            <?php
+                            $result_post = "SELECT * FROM `agendamentos_dos_servicos` "
+                                . "WHERE agendamentos_id = " . $la->getId();
+                            $resultado_post = mysqli_query($conn, $result_post);
+                            while ($row_post = mysqli_fetch_assoc($resultado_post)) {
+                                $func = $row_post['sf_funcionario'];
+
+                                $result_post02 = "Select * from `usuario` WHERE id = $func";
+                                $resultado_post02 = mysqli_query($conn, $result_post02);
+                                while ($row_post02 = mysqli_fetch_assoc($resultado_post02)) {
+                                    echo '<li style="list-style: none;">' . $row_post02['nome'] . '</li>';
+                                }
+                            }
+                            ?>
+                        </td>
+                        <td>
+                            <?php
+                            $result_post = "SELECT * FROM `agendamentos` "
+                                . "INNER JOIN usuario on agendamentos.usuario_id = usuario.id WHERE agendamentos.idAgendamento = " . $la->getId();
+                            $resultado_post = mysqli_query($conn, $result_post);
+                            while ($row_post = mysqli_fetch_assoc($resultado_post)) {
+                                echo '<li style="list-style: none;" >' . $row_post['nome'] . '</li>';
+                            }
+                            ?>
+                        </td>
+                    </tr>
+            <?php
+                }
+            }
+            ?>
         </tbody>
     </table>
 </body>
