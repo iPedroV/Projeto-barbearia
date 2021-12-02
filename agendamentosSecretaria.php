255f9d048d1fa77b1a40c94dbbf18e60b1004305
SamuelHFS <samuel.jpg1@gmail.com>
2021-12-02 10:35:45 -0300

    modal quase funcionando

--- agendamentosSecretaria.php
+++ agendamentosSecretaria.php
@@ -45,6 +45,15 @@ include_once 'C:/xampp/htdocs/Projeto-barbearia/model/mensagem.php';
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
     <script src="Js/sweetalert2.all.min.js"></script>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
     <title>Agendamentos</title>
 </head>
@@ -211,18 +220,22 @@ include_once 'C:/xampp/htdocs/Projeto-barbearia/model/mensagem.php';
                                     ?>
                                 </td>
                                 <td></td>
-                                <td>
-                                    <button type="button" class="btn btn-outline-danger" data-toggle="modal" data-target="#exampleModal">
+                               <td>
+                                    <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#ModalExcluir">
                                         Cancelar</button>
-                                </td>
+                               </td>
                                 <td>
                                     <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#exampleModal2">
                                         Concluir</button>
                                 </td>
                             </tr>
-
+                          
+                            
+                           
+                           
+                            </div>
                             <!--Modal de excluir o agendamento pela secretaria-->
-                            <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal fade" id="ModalExcluir" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
