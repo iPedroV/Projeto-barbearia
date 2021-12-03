10ce71501eaf9bc4879e3ea23c3283d05d73fb04
abreufabio <abreufabio71@oulook.com>
2021-12-03 13:26:00 -0300

    Mensagem de buenas ... :microbe:

--- agendamentosSecretaria.php
+++ agendamentosSecretaria.php
@@ -52,6 +52,8 @@ $dts = new AgendamentoController();
     <link rel="stylesheet" href="css/bootstrap.css">
     <link rel="stylesheet" href="css/bootstrap.min.css">
 
+    <script src="Js/sweetalert2.all.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
     <title>Agendamentos</title>
