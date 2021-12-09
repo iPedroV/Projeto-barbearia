76af80a1c3c8bde7a9ee5fd4ea8d656ef4629119
iPedroV <pedro.vcosta405@gmail.com>
2021-12-09 18:35:28 -0300

    Correção

--- cadastroNoticia.php
+++ cadastroNoticia.php
@@ -75,14 +75,14 @@ $msg = new Mensagem();
                 max-width: 100%;
                 position: fixed;
                 /* margin-left: 100%; */
-                margin-top: 71px;
+                margin-top: 11px;
             }
             header {
                 position: fixed !important;
                 top: 0 !important;
                 left: 0 !important;
                 width: 100% !important;
-                padding: 0px 42px !important;
+                padding: 30px 42px !important;
                 z-index: 10000 !important;
                 display: flex !important;
                 justify-content: space-between !important;
